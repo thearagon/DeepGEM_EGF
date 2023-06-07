af478749d22ebed7c2a14e69b19ec61a229560b9
Thea Ragon <tragon@caltech.edu>
2023-06-07 15:34:40 -0700

    up

--- deconvEgf_helpers.py
+++ deconvEgf_helpers.py
@@ -178,7 +178,7 @@ def MStep(k_egf, z_sample, x_sample, npix, npiy, ytrue, img_generator, kernel_ne
 
     ## Priors on init GF
     prior = args.prior_phi_weight * prior_phi[0](kernel.squeeze(0))
-    prior += prior_phi[1](kernel.squeeze(0), args.prior_phi_weight )[0]
+    prior += prior_phi[1](kernel, args.prior_phi_weight )[0]
 
     print('DTW : {}'.format(prior_phi[1](kernel.squeeze(0), args.prior_phi_weight ) ) )
     print('L2 : {}'.format(prior_phi[2](kernel.squeeze(0), args.prior_phi_weight ) ) )
@@ -336,7 +336,7 @@ def Loss_DTW(z, z0):
 def Loss_DTW_Mstep(z, z0):
     # uses fast DTW, similar to L2 if aligned
     sdtw = soft_dtw_cuda.SoftDTW(use_cuda= False, gamma=0.1)
-    return sdtw(z, z0)
+    return sdtw(z, z0.unsqueeze(0))
 
 def Loss_multicorr(z, args):
 
