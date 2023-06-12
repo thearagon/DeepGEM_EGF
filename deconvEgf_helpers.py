983de61656f7197df9182489b0b72894362840fe
Thea Ragon <tragon@caltech.edu>
2023-06-12 14:25:16 -0700

    up

--- deconvEgf_helpers.py
+++ deconvEgf_helpers.py
@@ -196,7 +196,7 @@ def MStep(k_egf, z_sample, x_sample, npix, npiy, ytrue, img_generator, kernel_ne
         # else:
         sdtw = soft_dtw_cuda.SoftDTW(use_cuda=False, gamma=1)
         multi_loss = args.egf_multi_weight * (Loss_L2(kernel.squeeze(0), mEGF_kernel_list[idx_best].squeeze(0))
-                                               + 0.35*sdtw(kernel.squeeze(0), mEGF_kernel_list[idx_best].squeeze(0))[0] ) + args.egf_multi_weight*1e-2* torch.sum( [Loss_L2(kernel.squeeze(0),e.squeeze(0)) for i, e in enumerate(mEGF_kernel_list) if i != idx_best])
+                                               + 0.35*sdtw(kernel.squeeze(0), mEGF_kernel_list[idx_best].squeeze(0))[0] ) + args.egf_multi_weight*1e-2* torch.sum( torch.Tensor([Loss_L2(kernel.squeeze(0),e.squeeze(0)) for i, e in enumerate(mEGF_kernel_list) if i != idx_best]) )
     else:
         multi_loss = torch.tensor(0.)
 
