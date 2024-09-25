f56fc641585c6def532dcab3aa83faea7f32f2d1
Thea Ragon <tragon@caltech.edu>
2024-09-25 18:17:42 +0200

    priors

--- GEMDeconvEgf.py
+++ GEMDeconvEgf.py
@@ -107,7 +107,6 @@ def main_function(args):
     gf0 = torch.Tensor(gf0).to(device=args.device)
 
     stf0 = stf0 / np.amax(stf0)
-    stf0 = torch.Tensor(stf0).to(device=args.device)
 
     trc0 /= np.amax(np.abs(trc0))
     trc0 = torch.Tensor(trc0).to(device=args.device)
@@ -168,8 +167,11 @@ def main_function(args):
     prior_stf = [prior_boundary, prior_TV_stf, stf_softl1]  # priors on STF, can be a list
 
     ## TODO
-    stf0_gauss = torch.abs(stf0 + torch.normal(torch.zeros(stf0.shape), args.stf0_sigma))
+    stf0_gauss = torch.abs(stf0 + torch.random.normal(0, args.stf0_sigma, stf0.shape))
+    stf0 = torch.Tensor(stf0).to(device=args.device)
+    stf0_gauss = torch.Tensor(stf0_gauss).to(device=args.device)
     stf0_gauss_ext = torch.unsqueeze(torch.cat(args.btsize * [torch.unsqueeze(stf0_gauss, axis=0)], axis=0), axis=1)
+    stf0_gauss_ext.to(device=args.device)
     prior_gauss = lambda stf: Loss_L2(stf, stf0_gauss_ext)/args.stf0_sigma**2
     prior_x = prior_gauss
 
